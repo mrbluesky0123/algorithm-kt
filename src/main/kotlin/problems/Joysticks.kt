c20176884e784310fb22bf6a862ed6f6a4cb03cb
mrbluesky <gogetit88@gmail.com>
2021-02-02 18:16:53 +0900

    TargetNumber

--- src/main/kotlin/problems/Joysticks.kt
+++ src/main/kotlin/problems/Joysticks.kt
@@ -2,25 +2,34 @@ package problems
 
 
 fun solution(name: String): Int {
-    val chars = charArrayOf('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
-
     var nameChars = name.toCharArray()
-    var cnt = 0
-    var currentIndex = 0
+    var moveCnt = 0
+    var prevIndex = 0
     var initChar = 'A'
-    for(curChar in nameChars) {
-        var back = ((initChar - curChar).toInt() + 26) % 26
-        var front =  ((curChar - initChar).toInt() + 26) % 26
-        cnt += Math.min(back, front)
+
+    for(curIndex in nameChars.indices) {
+        val curChar = nameChars[curIndex]
+        if(curChar == 'A') {
+            continue
+        }
+        // get digit movement
+        var left = ((prevIndex - curIndex) + nameChars.size) % nameChars.size
+        var right = ((curIndex - prevIndex) + nameChars.size) % nameChars.size
+        moveCnt += Math.min(left, right)
+
+        // get movement for changing chars
+        var down = ((initChar - curChar).toInt() + 26) % 26
+        var up =  ((curChar - initChar).toInt() + 26) % 26
+        moveCnt += Math.min(down, up)
+
+        prevIndex = curIndex
     }
 
-    return cnt + (nameChars.size-1)
-
+    return moveCnt
 }
 
 
 fun main(args: Array<String>) {
-    var name = "JAZ"
+    var name = "BBBBAAAB"
     println(solution(name))
 }
---
+++ src/main/kotlin/problems/Solution.kt
@@ -0,0 +1,13 @@
+package problems
+
+class Solution {
+    fun solution(numbers: IntArray, target: Int): Int {
+        var answer = 0
+
+        return answer
+    }
+
+    fun calculate() {
+
+    }
+}
