a6356b1a77e6975923f96bc125ab11031061bc56
mrbluesky0123 <onightraino@gmail.com>
2021-02-03 08:14:23 +0900

    Target number complete

--- src/main/kotlin/problems/Solution.kt
+++ src/main/kotlin/problems/Solution.kt
@@ -1,13 +1,27 @@
 package problems
 
 class Solution {
+    var cnt: Int = 0
+    var forDebug = Array<Int>(5){i -> 0}
     fun solution(numbers: IntArray, target: Int): Int {
-        var answer = 0
-
-        return answer
+//        var answer = 0
+        calculate(numbers, target, 0, 0)
+        return cnt
     }
 
-    fun calculate() {
-
+    fun calculate(numbers: IntArray, target: Int, currentIdx: Int, sum: Int) {
+        var signs = intArrayOf(-1, 1)
+        for(sign in signs) {
+            var s = sum + numbers[currentIdx] * sign
+//            forDebug[currentIdx] = numbers[currentIdx] * sign
+            if(currentIdx == numbers.size-1) {
+//                forDebug.iterator().forEach { print("$it ") }
+//                println()
+                if(s == target)
+                    cnt++;
+            } else {
+                calculate(numbers, target, currentIdx+1, s)
+            }
+        }
     }
 }
--- src/main/kotlin/problems/TargetNumber.kt
+++ src/main/kotlin/problems/TargetNumber.kt
@@ -1,34 +1,11 @@
 package problems
 
 
-fun solution(numbers: IntArray, target: Int): Int {
-    var answer = 0
-    answer = calculate(numbers, target, 0, 0)
-    return answer
-}
 
-fun calculate(numbers: IntArray, target: Int, currentIdx: Int, sum: Int): Int{
 
-    var signs = intArrayOf(-1, 1)
-    var cnt = 0
-    var s = sum
-    for(sign in signs) {
-        s = sum + numbers[currentIdx] * sign
-        if(currentIdx == numbers.size-1) {
-            if(s == target)
-                cnt += 1;
-            else
-                cnt = 0
-        } else {
-            cnt += calculate(numbers, target, currentIdx+1, s)
-        }
-    }
-
-    return cnt
-
-}
 
 
 fun main(args: Array<String>) {
-    println(solution(intArrayOf(1, 1, 1, 1, 1), 5))
+    var s = Solution()
+    println("#### " + s.solution(intArrayOf(1, 1, 1, 1, 1), 3))
 }
